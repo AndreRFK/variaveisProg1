aea65737dc82c2f0a24a6322635e2877581fefc8
alunoadm <alunoadm@MSCGRESL04007.estacio.acad>
2017-08-30 22:45:01 -0300

    EXERCICIO001

---
+++ src/br/estacio/com/Ex001.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.com;
+
+/**
+ *
+ * @author alunoadm
+ */
+public class Ex001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 2;
+        float y = 3.1f;
+        double z;
+        z = x * y;
+        
+
+        //sout CTRL + SPACE
+        System.out.println (z);
+    }
+    
+}
