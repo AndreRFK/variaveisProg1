f6826293d7df1bdff5d75c8e94118824d3c30ee6
alunoadm <alunoadm@MSCGRESL04005.estacio.acad>
2017-09-06 21:58:17 -0300

    Alteração no exemplo do Border Layout. Sufixo e prefixo

---
+++ src/br/estacio/com/SufixoEPrefixo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.com;
+
+/**
+ *
+ * @author alunoadm
+ */
+public class SufixoEPrefixo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int prefixo = 10;
+        int sufixo = 40;
+        
+        int diferencaPrefixo;
+    }
+    
+}
